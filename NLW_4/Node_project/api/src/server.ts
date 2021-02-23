6ca2913d1f81e7331f86c012938d581992a4e44c
matheusfilipebb <72322510+matheusfilipebb@users.noreply.github.com>
2021-02-23 17:03:45 -0300

    Revert "NLW#4 - Day 1"
    This reverts commit 2980b3acd31ca604f81e15450440f1bb7a64f5da.

--- NLW_4/Node_project/api/src/server.ts
+++
@@ -1,9 +0,0 @@
-import express from "express"
-
-const app = express()
-
-app.get("/", (req,res) => res.json({message: "Hey JSON is working!"}))
-
-app.post("/", (req,res) => res.json({message: "The data was successfully saved"}))
-
-app.listen(3333, () => console.log("Listening port 3333"))
